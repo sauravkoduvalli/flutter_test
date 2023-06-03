ec85513b513502deba055a513ec16a29306d4978
Saurav K <sauravkoduvalli@gmail.com>
2023-06-03 23:46:59 +0530

    print staments cleared

--- lib/bloc/home/home_bloc.dart
+++ lib/bloc/home/home_bloc.dart
@@ -7,11 +7,9 @@ part 'home_state.dart';
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(const HomeState()) {
     on<OnMenuChangeEvent>((event, emit) {
-      // print("current index is ${event.currentIndex}");
       emit(state.copyWith(
         currentMenu: event.currentIndex,
       ));
-      // print("current menu is ${state.currentMenu}");
     });
   }
 }
